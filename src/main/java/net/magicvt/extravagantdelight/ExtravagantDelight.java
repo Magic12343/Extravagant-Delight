31bbc5652f0e77250100e48e31798ebdba69e3b4
MagicVT <maxgamerspro@gmail.com>
2025-08-24 01:33:59 -0300

    More Coaster Compats.
    Added in Built-in Resource Pack:
    -Peculiars
    -Seasonals

--- src/main/java/net/magicvt/extravagantdelight/ExtravagantDelight.java
+++ src/main/java/net/magicvt/extravagantdelight/ExtravagantDelight.java
@@ -14,6 +14,8 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -25,17 +27,17 @@ public class ExtravagantDelight {
     public static final String MOD_ID = "extravagantdelight";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-
     public ExtravagantDelight() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
 
         ModCreativeTab.register(modEventBus);
         ModItems.register(modEventBus);
         ModBlocks.register(modEventBus);
+
         HHCompatFluids.FLUID_TYPES.register(modEventBus);
         HHCompatFluids.FLUIDS.register(modEventBus);
         HHCompatFluids.registerCompat();
+
         BCCompatFluids.FLUID_TYPES.register(modEventBus);
         BCCompatFluids.FLUIDS.register(modEventBus);
         BCCompatFluids.registerCompat();
@@ -43,11 +45,12 @@ public class ExtravagantDelight {
         CompatHandler.init(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
+
         MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-
     }
 
     @SubscribeEvent
@@ -59,6 +62,7 @@ public class ExtravagantDelight {
     public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
+
             ItemBlockRenderTypes.setRenderLayer(ModBlocks.STARFRUIT_CROP.get(), RenderType.cutout());
         }
     }
